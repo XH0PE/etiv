f702aa6b0b2e36c2269951be80ab08e0ed4a27ce
Diego <pecioso999@gmail.com>
2023-10-18 19:50:59 -0500

    'casi'

--- mern/server/app.js
+++ mern/server/app.js
@@ -9,6 +9,7 @@ const app = express()
 // Importacion de rutas
 const authRoutes = require("./router/auth.router")
 const userRoutes = require("./router/user.router")
+const menuRoutes = require("./router/menu.router")
 
 // Configurar Body Parser
 app.use(bodyParser.urlencoded({extended: true}))
@@ -23,5 +24,6 @@ app.use(cors())
 // Configurar Rutas
 app.use(`/api/${API_VERSION}`, authRoutes)
 app.use(`/api/${API_VERSION}`, userRoutes)
+app.use(`/api/${API_VERSION}`, menuRoutes)
 
 module.exports = app 
--- mern/server/controllers/user.controller.js
+++ mern/server/controllers/user.controller.js
@@ -1,4 +1,6 @@
+const bcrypt = require("bcryptjs")
 const User = require("../models/user.model")
+const image = require("../utils/image")
 
 async function getMe(req, res) {
 
@@ -25,16 +27,79 @@ async function getUsers(req, res){
     }
 
     res.status(200).send(response)
-
 }
 
 async function createUser (req, res){
-    console.log(req.body)
+
+    const { password } = req.body
+    const user = new User({ ...req.body, active: false })
+
+    const salt = bcrypt.genSaltSync(10)
+    const hashPassword = bcrypt.hashSync(password, salt)
+
+    user.password = hashPassword
+
+    if(req.files.avatar){
+        const imagePath = image.getFilePath(req.files.avatar)
+        user.avatar = imagePath
+    }
+
+    user.save((error, userStored) => {
+        if (error){
+            res.status(400).send({msg: "Error al crear el usuario !"})
+        } else {
+            res.status(201).send(userStored)
+        }
+    })
+
     res.status(200).send({msg: "Funciona!"})
+}
+
+async function updateUser(req, res) {
+    const { id } = req.params
+    const userData = req.body
+
+    //Password
+    if (userData.password) {
+        const salt = bcrypt.genSaltSync(10)
+        const hashPassword = bcrypt.hashSync(userData.password, salt)
+        userData.password = hashPassword
+    } else {
+        delete userData.password
+    }
+
+    //Avatar
+    if (req.files.avatar) {
+        const imagePath = image.getFilePath(req.files.avatar)
+        userData.avatar = imagePath
+    }
+
+    User.findByIdAndUpdate({ _id: id}, userData, (error) => {
+        if(error) {
+            res.status(400).send({msg: "Error al actualizar el usuario"})
+        } else {
+            res.status(200).send({msg: "Datos de usuario actualizados*"})
+
+        }
+    })
+}
+
+async function deleteUser(req, res){
+    const { id } = req.params
+
+    User.findByIdAndDelete(id, (error) => {
+        if(error){
+            res.status(400).send({msg: "Error al eliminar el usuario"})
+        } else {
+            res.status(200).send({msg: "Usuario eliminado"})
+        }
+    })
 }
 
 module.exports = {
     getMe,
     getUsers,
-    createUser
+    createUser,
+    updateUser,
+    deleteUser,
 }
