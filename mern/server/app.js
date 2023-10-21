8a68b9844b91440a3c0cac88ddb9f3678a56658b
Diego <pecioso999@gmail.com>
2023-10-20 19:58:20 -0500

    'finish_V'

--- mern/server/app.js
+++ mern/server/app.js
@@ -11,6 +11,8 @@ const authRoutes = require("./router/auth.router")
 const userRoutes = require("./router/user.router")
 const menuRoutes = require("./router/menu.router")
 const courseRoutes = require("./router/course.router")
+const postRoutes = require("./router/post.router")
+const newsletterRoutes = require("./router/newsletter.router")
 
 // Configurar Body Parser
 app.use(bodyParser.urlencoded({extended: true}))
@@ -27,5 +29,7 @@ app.use(`/api/${API_VERSION}`, authRoutes)
 app.use(`/api/${API_VERSION}`, userRoutes)
 app.use(`/api/${API_VERSION}`, menuRoutes)
 app.use(`/api/${API_VERSION}`, courseRoutes)
+app.use(`/api/${API_VERSION}`, postRoutes)
+app.use(`/api/${API_VERSION}`, newsletterRoutes)
 
 module.exports = app 
